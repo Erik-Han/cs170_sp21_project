c0aed544789afe10071ad6b5a19262f7b6ac4b38
Erik-Han <erik.y.han@gmail.com>
2021-04-30 22:40:10 -0400

    update

--- part2.py
+++ part2.py
@@ -2,12 +2,14 @@ import numpy as np
 import os
 from parse import read_input_file
 import networkx as nx
+import copy
 from networkx.exception import NetworkXNoPath
-ans = -1
-ans_details = None
-
+import matplotlib.pyplot as plt
 
 def find_longest_path_setup(G):
+    nx.draw(G, with_labels = True)
+    plt.savefig("start.png")
+    plt.clf()
     size = G.number_of_nodes()
     k = 15
     c = 1
@@ -21,44 +23,75 @@ def find_longest_path_setup(G):
         k = 100
         c = 5
     ans_list = [[], []]
-    find_longest_path(G, 1, 1, ans_list)
+    dpth = 1
+    for i in range(k+c):
+        curr = find_longest_path(G, k, c, dpth, True)
+        curr_length = nx.shortest_path_length(G, source=0, target=size-1, weight="weight")
+        print(curr_length)
+        if not curr:
+            break
+        if type(curr) == tuple:
+            k -= 1
+            ans_list[0].append(curr)
+            G.remove_edge(curr[0], curr[1])
+        elif type(curr) == int:
+            c -= 1
+            ans_list[1].append(curr)
+            G.remove_node(curr)
+        else:
+            print("BAD")
+            return
+    # G.remove_node(1)
+    # shortest_path = nx.shortest_path(G, source=0, target=size- 1, weight="weight")
+    nx.draw(G, with_labels = True)
+    plt.savefig("end.png")
+    return ans_list
 
 
-def find_longest_path(G, k, c, ans_list):
+
+def find_longest_path(G, k, c, depth, selection):
+    num_nodes = G.number_of_nodes()
     try:
-        poss_ans = nx.shortest_path_length(G, source=0, target=29, weight="weight")
-        print(poss_ans)
+        poss_ans = nx.shortest_path_length(G, source=0, target=num_nodes-1, weight="weight")
     except NetworkXNoPath:
         return
     global ans
-    if poss_ans > ans:
-        ans = max(ans, poss_ans)
-        global ans_details
-        ans_details = list(ans_list)
-    shortest_path = nx.shortest_path(G, source=0, target=29, weight="weight")
-    print(shortest_path)
+
+    if depth == 0:
+        return poss_ans
+    shortest_path = nx.shortest_path(G, source=0, target=num_nodes-1, weight="weight")
 
     # try removing each edge along the path
+    longest_path_length = poss_ans
+    if selection:
+        best_item = None
     if k > 0:
         for i in range(len(shortest_path) - 1):
             tmp = G[shortest_path[i]][shortest_path[i + 1]]["weight"]
             G.remove_edge(shortest_path[i], shortest_path[i + 1])
-            ans_list[0].append((shortest_path[i], shortest_path[i + 1]))
-            find_longest_path(G, k - 1, c, ans_list)
-            del ans_list[0][-1]
+            curr_path_length = find_longest_path(G, k - 1, c, depth - 1, False)
+            if curr_path_length and curr_path_length > longest_path_length:
+                longest_path_length = curr_path_length
+                if selection:
+                    best_item = (shortest_path[i], shortest_path[i+1])
             G.add_edge(shortest_path[i], shortest_path[i + 1], weight=tmp)
-
     # try removing each node along the path
     if c > 0:
         for i in range(1, len(shortest_path) - 1):
             tmp = list(G.edges(shortest_path[i], data="weight"))
             G.remove_node(shortest_path[i])
-            ans_list[1].append(shortest_path[i])
-            find_longest_path(G, k, c - 1, ans_list)
-            del ans_list[1][-1]
+            curr_path_length = find_longest_path(G, k, c - 1, depth - 1, False)
+            if curr_path_length and curr_path_length > longest_path_length:
+                longest_path_length = curr_path_length
+                if selection:
+                    best_item = shortest_path[i]
+
             G.add_node(shortest_path[i])
             for edge in tmp:
                 G.add_edge(edge[0], edge[1], weight=edge[2])
+    if selection:
+        return best_item
+    return longest_path_length
 
 
 
@@ -67,8 +100,8 @@ if __name__ == "__main__":
         min_size = 29
         max_size = 30
         graph = read_input_file("./inputs/"+file, min_size=min_size, max_size=max_size)
-        find_longest_path_setup(graph)
-        print(ans)
-        print(ans_details)
+        ans = find_longest_path_setup(graph)
+        print("ANSWER:", ans)
+
 
 #6:54
--- testing.py
+++ testing.py
@@ -3,3 +3,13 @@ if __name__ == "__main__":
     print([1,2,3] in list)
     print([1, 3] in list)
     print([1, [2,3]] in list)
+
+    lst = [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]]
+    lst2 = lst[0]
+    print(lst)
+    print(lst2)
+    lst2[0] = 10000
+    lst[0] = None
+    print(lst)
+
+    print(lst2)
